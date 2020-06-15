0e3de15bb3d282b3dc27d7f4f372f3de756a4103
Jan van Heesch <Jan.vanHeesch@gmail.com>
2020-06-15 19:29:42 +0200

    Added missing file

---
+++ ACHNBrowserUI/ACNHBrowserUIWatch Extension/views/dodocode/DodoCodeWatchRow.swift
@@ -0,0 +1,20 @@
+//
+//  DodoCodeWatchRow.swift
+//  ACNHBrowserUIWatch Extension
+//
+//  Created by Jan on 15.06.20.
+//  Copyright © 2020 Thomas Ricouard. All rights reserved.
+//
+
+import SwiftUI
+import Backend
+
+struct DodoCodeWatchRow: View {
+    let code: DodoCode
+
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
